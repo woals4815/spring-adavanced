f1df26ac4a10afed9baab6ac20bdf348a40fecf7
woals4815 <ckrt4815@gmail.com>
2022-05-23 21:19:45 +0900

    [Feature] add ThreadLocal

---
+++ src/test/java/hello/advanced/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,41 @@
+package hello.advanced.threadlocal;
+
+import hello.advanced.threadlocal.code.FieldService;
+import hello.advanced.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+    private ThreadLocalService fieldService = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+        
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB= new Thread(userB);
+        threadB.setName("thread-B");
+        
+        threadA.start();
+        sleep(100);
+        threadB.start();
+        sleep(2000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
