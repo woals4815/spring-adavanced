8e716bb8dd4019721d81a75fbf1ad87d925c74de
woals4815 <ckrt4815@gmail.com>
2022-05-23 21:06:18 +0900

    [Feature] add thread concurrency problem in test

---
+++ src/main/java/hello/advanced/trace/logtrace/LogTraceConfig.java
@@ -0,0 +1,13 @@
+package hello.advanced.trace.logtrace;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogTraceConfig {
+
+    @Bean
+    public LogTrace logTrace() {
+        return new FieldLogTrace();
+    }
+}
